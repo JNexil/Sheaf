c9b123f1e904169d3441fb997c808b94071b9ef4
Jamefrus <jamefrus@ya.ru>
2016-10-23 09:01:56 +0300

    Updated generators: 1) multi (likely to past util, with custom name) 2) flat (just write file with replacements)

--- generator/src/main/kotlin/su/jfdev/codegen/generators/Generators.kt
+++ generator/src/main/kotlin/su/jfdev/codegen/generators/Generators.kt
@@ -4,12 +4,28 @@ import java.io.*
 
 object Generators {
     fun util(resource: File) = util { resource.readText() }
-    fun util(resource: () -> String) = Generator {
-        multifile(it.pack.name + "Util")
+    fun util(resource: () -> String) = multi {
+        pack.name + "Util" to resource()
+    }
+
+    inline fun multi(resource: File, crossinline name: Gen.() -> String) = multi {
+        name() to resource.readText()
+    }
+
+    inline fun multi(crossinline nameWithResource: Gen.() -> Pair<String, String>) = Generator {
+        val (name, resource) = nameWithResource(it)
+        multifile(name)
         suppress("NOTHING_TO_INLINE")
         _package()
+        +""
         author()
         +""
-        +resource()
+        +resource
+    }
+
+    fun flat(file: File) = Generator {
+        file.forEachLine {
+            +it
+        }
     }
 }
